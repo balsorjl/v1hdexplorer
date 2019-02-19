756e9e56c275b6e239d43e0bac41ee43005ac6df
Justin Balsor <47674483+balsorjl@users.noreply.github.com>
2019-02-19 15:30:10 -0700

    Add files via upload

--- R/v1hdexplorer.R
+++ R/v1hdexplorer.R
@@ -3,30 +3,28 @@ library("easypackages")
 
 #' Installs Packages
 #'
-#' @param all.packages Installs all available packages
-#' @param some.packages Installs only required packages
+#' @param some.packages Installs only required packages. These include: corrplot,dendextend,factoextra,FactoMineR,ggplot2,gplots,Hmisc,psych,RColorBrewer,RSKC,Rtsne,seriation,sparcl,stats,subspace,tsne, vioplot.
+#' @param all.packages Installs all packages contained in 'some.packages' as well as additional recommended packages. Additional packages include: autoimage,data.table,d3heatmap,dendsort,GGally,ggpubr,ggrepel,gridExtra,miscTools,plotrix,plyr,reshape, scales
 #'
-#' @return
-#' @export
+#' @return This will consult CRAN for the desired set of packages and install them in your package library. Some packages will ask if you would like to compile them (y/n), and the install process takes some time.
 #'
-#' @examples
-install.HDpackages<- function(all.packages,some.packages){
-  all.packages<-c("pvclust","ggplot2","gridExtra","ggrepel","Rtsne","scatterplot3d","plotly","plotrix","gplots","ggpubr","plyr","miscTools","autoimage","clValid","optCluster","RankAggreg","MBCluster.Seq","RColorBrewer","data.table","vioplot","d3heatmap","seriation","dendsort","dendextend","Hmisc","rgl","plotly","MBCluster.Seq","sparcl","RSKC","mice","Rtsne","ggrepel","GGally","reshape","scales","gridExtra","FactoMineR","factoextra","corrplot","psych","FactoInvestigate")
-  some.packages<-c("ggplot2")
-  install_packages(all.packages,some.packages)
+#' @examples install.HDpackages(some.packages=TRUE) #Will install the packages in some.packages
+install.HDpackages<- function(some.packages,all.packages){
+  some.packages<-c("corrplot","dendextend","factoextra","FactoMineR","ggplot2","gplots","Hmisc","psych","RColorBrewer","RSKC","Rtsne","seriation","sparcl","stats","subspace","tsne","vioplot")
+  all.packages<-c("corrplot","dendextend","factoextra","FactoMineR","ggplot2","gplots","Hmisc","psych","RColorBrewer","RSKC","Rtsne","seriation","sparcl","stats","subspace","tsne","vioplot","autoimage","data.table","d3heatmap","dendsort","GGally","ggpubr","ggrepel","gridExtra","miscTools","plotrix","plyr","reshape","scales")
+  install_packages(some.packages,all.packages)
 }
 
 #' Loads Packages
 #'
-#' @param all.packages Loads all available packages
-#' @param some.packages Loads only required packages
+#' @param some.packages Loads only required packages. These include: corrplot,dendextend,factoextra,FactoMineR,ggplot2,gplots,Hmisc,psych,RColorBrewer,RSKC,Rtsne,seriation,sparcl,stats,subspace,tsne, vioplot.
+#' @param all.packages Loads all packages contained in 'some.packages' as well as additional recommended packages. Additional packages include: autoimage,data.table,d3heatmap,dendsort,GGally,ggpubr,ggrepel,gridExtra,miscTools,plotrix,plyr,reshape, scales
 #'
-#' @return
-#' @export
+#' @return This will consult your library and load the desired set of packages (some.packages or all.packages).
 #'
-#' @examples
-load.HDpackages<- function(all.packages,some.packages){
-  all.packages<-c("pvclust","ggplot2","gridExtra","ggrepel","Rtsne","scatterplot3d","plotly","plotrix","gplots","ggpubr","plyr","miscTools","autoimage","clValid","optCluster","RankAggreg","MBCluster.Seq","RColorBrewer","data.table","vioplot","d3heatmap","seriation","dendsort","dendextend","Hmisc","rgl","plotly","MBCluster.Seq","sparcl","RSKC","mice","Rtsne","ggrepel","GGally","reshape","scales","gridExtra","FactoMineR","factoextra","corrplot","psych","FactoInvestigate")
-  some.packages<-c("ggplot2")
-  libraries(all.packages,some.packages)
+#' @examples load.HDpackages(some.packages=TRUE) #Will load the packages in some.packages to your library
+load.HDpackages<- function(some.packages,all.packages){
+  some.packages<-c("corrplot","dendextend","factoextra","FactoMineR","ggplot2","gplots","Hmisc","psych","RColorBrewer","RSKC","Rtsne","seriation","sparcl","stats","subspace","tsne","vioplot")
+  all.packages<-c("corrplot","dendextend","factoextra","FactoMineR","ggplot2","gplots","Hmisc","psych","RColorBrewer","RSKC","Rtsne","seriation","sparcl","stats","subspace","tsne","vioplot","autoimage","data.table","d3heatmap","dendsort","GGally","ggpubr","ggrepel","gridExtra","miscTools","plotrix","plyr","reshape","scales")
+  libraries(some.packages,all.packages)
 }
